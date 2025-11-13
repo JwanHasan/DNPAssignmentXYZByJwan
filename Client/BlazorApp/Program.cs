fa7127cd104b108163748b2eec2710202bd80a3e
Jwan Hasan <cloudcaelum22@outlook.com>
2025-11-13 23:25:35 +0100

    starting with razor web interface

--- Client/BlazorApp/Program.cs
+++ Client/BlazorApp/Program.cs
@@ -1,4 +1,5 @@
 using BlazorApp.Components;
+using BlazorApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +11,10 @@ builder.Services.AddScoped(sp => new HttpClient
     BaseAddress = new Uri ("https://localhost:5209/")
 }
 );
-
-
+// Dependency Injection for Services
+builder.Services.AddScoped<IUserService, HttpUserService>();
+builder.Services.AddScoped<IPostService, HttpPostService>();
+builder.Services.AddScoped<ICommentService, HttpCommentService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
